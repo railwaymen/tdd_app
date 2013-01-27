801fd323dafbee631c6b9e69f2a6ee6557658c50
Michał Krajewski <mkrajewski90@gmail.com>
2013-01-27 17:09:58 +0100

    fix unit test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "present username" do
-    user = User.new
+    user = User.new(email: "user@example.com")
     user.name = ""
     assert !user.valid?, "Is valid with blank username"
     user.name = "Tom"
-    assert user.valid?
+    assert user.valid?, "Is valid with blank username"
   end
 end
